4bcca6c2c36f264d1330d23723db1522e3bffb1b
huy-developer-code <voquanghuy862001@email.com>
2022-04-25 17:20:36 +0700

    upload3

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// ok
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
